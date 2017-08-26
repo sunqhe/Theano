60c3158ac4937ece63f598d5695cd6692b5420f7
sunqhe <sunqhe@yahoo.com>
2017-08-26 16:33:08 -0400

    Add files via upload

---
+++ getting_started_theano.py
@@ -0,0 +1,9 @@
+import theano
+from theano import tensor as T
+
+a = T.scalar()
+b = T.scalar()
+y = a*b
+multiply = theano.function(inputs=[a,b], outputs=y)
+print multiply(3,2)
+print multiply(4,5)
